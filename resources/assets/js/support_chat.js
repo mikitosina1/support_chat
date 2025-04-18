95818bd6a8d025bfd5835277d3490f8ce0d15b14
mikitosina1 <mikitosina@mail.ru>
2025-04-18 20:00:11 +0200

    feat: retry websocket installation

--- resources/assets/js/support_chat.js
+++ resources/assets/js/support_chat.js
@@ -1,13 +1,26 @@
 import axios from 'axios';
 import $ from 'jquery';
-
+import Echo from 'laravel-echo';
+import Pusher from "pusher-js";
+window.Pusher = Pusher;
 
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.$ = $;
 
 $(document).ready(function () {
-	console.log("Support chat online");
+
+	window.Echo = new Echo({
+		broadcaster: 'pusher',
+		key: 'local',
+		wsHost: 'ws.ddev.site',
+		cluster: 'mt1',
+		wsPort: 6001,
+		wssPort: 6001,
+		forceTLS: false,
+		encrypted: false,
+		disableStats: true,
+	});
 
 	let closeBtn = $("#chat-header");
 	let supportChat = $("#support-chat");
@@ -24,4 +37,6 @@ $(document).ready(function () {
 			supportChatBtn.removeClass('close-btn');
 		}
 	});
+
+	console.log("Support chat online");
 });
