24c70bc52268c58dc2de991b59a0ec45ab49535c
mikitosina1 <mikitosina@mail.ru>
2025-04-22 01:07:39 +0200

    feat: style tiny fixes, text example placer changed

--- resources/views/components/supportchat.blade.php
+++ resources/views/components/supportchat.blade.php
@@ -10,10 +10,10 @@
 	</div>
 	<div id="chat-body">
 		<div class="chat-message user-message">
-			<div class="message-bubble">Привет, у меня вопрос по заказу</div>
+			<div class="message-bubble">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
 		</div>
 		<div class="chat-message support-message">
-			<div class="message-bubble">Здравствуйте! Чем могу помочь?</div>
+			<div class="message-bubble">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
 		</div>
 	</div>
 	<div id="chat-footer">
