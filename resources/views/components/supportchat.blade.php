57d1bfc1c8d138ea935048ea64a2c5f41138ca93
mikitosina1 <mikitosina@mail.ru>
2025-04-29 21:11:25 +0200

    feat: restyling button

--- resources/views/components/supportchat.blade.php
+++ resources/views/components/supportchat.blade.php
@@ -27,6 +27,12 @@
 	</div>
 	<div id="chat-footer">
 		<input type="text" id="chat-input" placeholder="{{ __('supportchat::chat_lang.placeholder') }}">
-		<button id="send-btn" class="dark:text-gray-300">@lang('supportchat::chat_lang.send')</button>
+		<button id="send-btn" class="dark:text-gray-300">
+			<svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+				 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+				<line x1="22" y1="2" x2="11" y2="13" />
+				<polygon points="22 2 15 22 11 13 2 9 22 2" />
+			</svg>
+		</button>
 	</div>
 </div>
