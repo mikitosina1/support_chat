0162de1936d34eea4f8ad6fcbe83a2a40733d762
mikitosina1 <mikitosina27@gmail.com>
2026-08-23 22:51:25 +0200

    feat: work on permission and access functional, added main permissions

---
+++ App/Permissions/SupportChatPermissions.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Modules\SupportChat\App\Permissions;
+
+use App\Contracts\ModulePermissions;
+
+final class SupportChatPermissions implements ModulePermissions
+{
+    public static function all(): array
+    {
+        return [
+            'access',
+            'view',
+            'send_message',
+            'close_room',
+        ];
+    }
+}
