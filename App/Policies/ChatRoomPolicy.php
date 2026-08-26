933425581b5bc6007bdeda9ddc7bf510e72f05d6
mikitosina1 <mikitosina27@gmail.com>
2026-08-26 17:40:12 +0200

    feat: modules rules and permissions skeleton to register and use roles

--- App/Policies/ChatRoomPolicy.php
+++ App/Policies/ChatRoomPolicy.php
@@ -7,8 +7,10 @@ use Modules\SupportChat\App\Models\ChatRoom;
 
 class ChatRoomPolicy
 {
-    public function before(User $user, string $ability): ?bool
-    {
+    public function before(
+        User $user,
+        string $ability
+    ): ?bool {
         if ($user->isAdmin()) {
             return true;
         }
@@ -16,40 +18,52 @@ class ChatRoomPolicy
         return null;
     }
 
-    public function viewAny(User $user): bool
-    {
+    public function viewAny(
+        User $user
+    ): bool {
         return false;
     }
 
-    public function view(User $user, ChatRoom $room): bool
-    {
+    public function view(
+        User $user,
+        ChatRoom $room
+    ): bool {
         return $this->isParticipant($user, $room);
     }
 
-    protected function isParticipant(User $user, ChatRoom $room): bool
-    {
+    protected function isParticipant(
+        User $user,
+        ChatRoom $room
+    ): bool {
         return $room->users()
             ->whereKey($user->id)
             ->exists();
     }
 
-    public function create(User $user): bool
-    {
+    public function create(
+        User $user
+    ): bool {
         return true;
     }
 
-    public function resolve(User $user, ChatRoom $room): bool
-    {
+    public function resolve(
+        User $user,
+        ChatRoom $room
+    ): bool {
         return $room->isOpen() && $this->isParticipant($user, $room);
     }
 
-    public function close(User $user, ChatRoom $room): bool
-    {
+    public function close(
+        User $user,
+        ChatRoom $room
+    ): bool {
         return $user->isAdmin() && ! $room->isClosed();
     }
 
-    public function sendMessage(User $user, ChatRoom $room): bool
-    {
+    public function sendMessage(
+        User $user,
+        ChatRoom $room
+    ): bool {
         return $room->isOpen()
             && $this->isParticipant($user, $room);
     }
